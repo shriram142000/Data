8d72c9fc549145048f9411ca7ab1bf6b4f95a681
shriram142000 <119671986+shriram142000@users.noreply.github.com>
2025-02-28 16:27:23 +0530

    added java file

---
+++ output3.java
@@ -0,0 +1,40 @@
+```java
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Choose an operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+        double result;
+        
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation");
+                return;
+        }
+        System.out.println("The result is: " + result);
+    }
+}
+```
